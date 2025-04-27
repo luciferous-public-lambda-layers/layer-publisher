8e6ef1f8fea55172ef347ba6a52645792ded79d0
sinofseven <em.s.00001@gmail.com>
2025-04-28 04:32:00 +0900

    impl

--- Makefile
+++ Makefile
@@ -7,15 +7,24 @@ format:
 update-failed:
 	poetry run python src/set_failed_state.py
 
+publish-start-deploy:
+	poetry run python src/publish/start_deploy.py
+
 publish-finish-deploy:
-	poetry run python src/publish/fnish_deploy/finish_deploy.py
+	poetry run python src/publish/finish_deploy.py
+
+generate-start-generate:
+	poetry run python src/generate/start_generate.py
 
 generate-complete-generate:
 	poetry run python src/generate/complete_generate.py
 
 
+
 .PHONY: \
 	format \
 	update-failed \
+	publish-start-deploy \
 	publish-finish-deploy \
+	generate-start-generate \
 	generate-complete-generate
---
+++ src/generate/start_generate.py
@@ -0,0 +1,48 @@
+from __future__ import annotations
+
+import json
+from datetime import datetime
+from typing import TYPE_CHECKING
+from zoneinfo import ZoneInfo
+
+import boto3
+from pydantic_settings import BaseSettings
+
+if TYPE_CHECKING:
+    from mypy_boto3_dynamodb.service_resource import Table
+
+
+class EnvironmentVariables(BaseSettings):
+    table_name: str
+    identifier: str
+    url_action_run: str
+
+
+jst = ZoneInfo("Asia/Tokyo")
+env = EnvironmentVariables()
+
+table: Table = boto3.resource("dynamodb").Table(env.table_name)
+
+
+def main():
+    update_state()
+
+
+def update_state():
+    attributes = {
+        "stateGenerate": "DEPLOYING",
+        "updatedAt": datetime.now(jst).isoformat(),
+    }
+    resp = table.update_item(
+        Key={"identifier": env.identifier},
+        UpdateExpression="set "
+        + ", ".join([f"#{k} = :{k}" for k in attributes.keys()]),
+        ExpressionAttributeNames={f"#{k}": k for k in attributes.keys()},
+        ExpressionAttributeValues={f"#{k}": v for k, v in attributes.items()},
+        ReturnValues="ALL_NEW",
+    )
+    return resp["Attributes"]
+
+
+if __name__ == "__main__":
+    main()
