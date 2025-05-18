44d1549b0c7b63ba5d667200d8baa9912dce95dd
sinofseven <em.s.00001@gmail.com>
2025-05-19 04:06:34 +0900

    impl

--- src/layer_publisher/generate/complete_generate.py
+++ src/layer_publisher/generate/complete_generate.py
@@ -33,11 +33,16 @@ class Layer(BaseModel):
     created_at: str
     region: str
 
+    def parse_runtime(self) -> int:
+        version = self.runtime[6:]
+        major, minor = [int(x) for x in version.split(".")]
+        return major * 1000 + minor
+
     @property
-    def sort_key(self) -> tuple[float, int, str]:
+    def sort_key(self) -> tuple[int, int, str]:
         mapping_arch = {"arm64,x86_64": 0, "x86_64": 1, "arm64": 2}
 
-        version = float("-" + self.runtime[6:])
+        version = self.parse_runtime()
         arch = mapping_arch[",".join(sorted(self.architectures))]
         return version, arch, self.region
 
